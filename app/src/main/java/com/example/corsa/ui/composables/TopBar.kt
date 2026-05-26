c1dc0d37b9c7209b7ab2c388f6fdfa3d8c9f8149
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-05-26 13:58:28 +0200

    refactor: use AppBarText for TopBar

--- app/src/main/java/com/example/corsa/ui/composables/TopBar.kt
+++ app/src/main/java/com/example/corsa/ui/composables/TopBar.kt
@@ -1,80 +1,22 @@
 package com.example.corsa.ui.composables
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar( navController: NavController)  {
-//    Row(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(horizontal = 8.dp, vertical = 12.dp),
-//        horizontalArrangement = Arrangement.SpaceBetween,
-//        verticalAlignment = Alignment.CenterVertically,
-//    ) {
-//        IconButton(onClick = {  }) {
-//            Icon(
-//                imageVector = Icons.Filled.Menu,
-//                contentDescription = "Menu",
-//                tint = cs.onBackground,
-//                modifier = Modifier.size(26.dp),
-//            )
-//        }
-//
-//
-//
-//        Box(
-//            modifier = Modifier
-//                .size(40.dp)
-//                .clip(CircleShape)
-//                .background(cs.secondary)
-//                .clickable(onClick = {  }),
-//            contentAlignment = Alignment.Center,
-//        ) {
-//            Icon(
-//                imageVector = Icons.Filled.Person,
-//                contentDescription = "Profile",
-//                tint = cs.onBackground,
-//                modifier = Modifier.size(22.dp),
-//            )
-//        }
-//    }
     CenterAlignedTopAppBar(
-        title = {
-            Text(
-                text = "Korsa",
-                fontWeight = FontWeight.ExtraBold,
-                fontStyle = FontStyle.Italic,
-                fontSize = 28.sp,
-                letterSpacing = 2.sp,
-            )
-        },
+        title = { AppBarText() },
 
         navigationIcon = {
             IconButton(onClick = {  }) {
